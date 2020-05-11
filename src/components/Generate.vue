13f212341a15d95d08425ee639c7a44d5b7b0923
Wietse Wind <mail@wietse.com>
2020-05-11 03:15:36 +0200

    Display family Seed as well & copy formatting

--- src/components/Generate.vue
+++ src/components/Generate.vue
@@ -15,7 +15,7 @@
       <br />
       <b>Account secret (for your eyes only)</b>
       <br />
-      <code class="generated font-weight-bold px-2 text-danger d-inline-block mb-2" v-clipboard:copy="'Secret (keep safe!): ' + secret.join(' ')">
+      <code class="generated font-weight-bold px-2 text-danger d-inline-block mb-2" v-clipboard:copy="'Secret Numbers (keep safe!): ' + secret.join(' ')">
         <small><small class="text-muted">A</small></small>{{ secret[0] }}
         <small><small class="text-muted">B</small></small>{{ secret[1] }}
         <small><small class="text-muted">C</small></small>{{ secret[2] }}
@@ -25,6 +25,14 @@
         <small><small class="text-muted">G</small></small>{{ secret[6] }}
         <small><small class="text-muted">H</small></small>{{ secret[7] }}
       </code>
+      <br />
+      <small>
+        - or - <b>Family Seed</b> (Alternative but common secret format)
+        <br />
+        <code class="generated px-2 text-danger d-inline-block mb-2" v-clipboard:copy="'Family Seed Secret (keep safe!): ' + familySeed">
+          {{ familySeed }}
+        </code>
+      </small>
     </div>
     <div class="mt-4 text-center d-print-none">
       <button @click="print()" class="font-weight-bold btn btn btn-dark">Print this (unsafe!) <small>- Better write down &amp; test</small></button>
@@ -60,13 +68,15 @@ export default {
         const account = new Account(Buffer.from(this.hash.slice(0, 32), 'hex'))
         this.secret = account.secret
         this.account = account.account.address
+        this.familySeed = account.account.familySeed
       }, 250)
     }
   },
   data () {
     return {
       account: 'XXXXXXXXXXXXXXXXXXXXXXXXXXX',
-      secret: ['XXXXXX', 'XXXXXX', 'XXXXXX', 'XXXXXX', 'XXXXXX', 'XXXXXX', 'XXXXXX', 'XXXXXX']
+      secret: ['XXXXXX', 'XXXXXX', 'XXXXXX', 'XXXXXX', 'XXXXXX', 'XXXXXX', 'XXXXXX', 'XXXXXX'],
+      familySeed: 'XXXXXXXXXXXXXXXXXXXXXXXXXXX'
     }
   },
   computed: {
@@ -89,14 +99,21 @@ export default {
       outline: 0;
       box-shadow: 0 0 0 3px rgba(0, 123, 255, .5);
       border-radius: 4px;
+      z-index: 10;
+      position: relative;
       &:before {
         content: 'Copied! (Unsafe!)';
+        background-color: rgba(200, 0, 0, .9);
+        padding: 1px 4px;
+        border-radius: 4px;
         display: block;
         position: absolute;
-        color: black;
+        color: white;
+        font-weight: bold;
+        border: 1px solid rgba(0, 0, 0, .5);
         font-size: 12px;
-        margin-top: -22px;
-        margin-left: -8px;
+        margin-top: -28px;
+        margin-left: -10px;
         letter-spacing: normal;
       }
     }
